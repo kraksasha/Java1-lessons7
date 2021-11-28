8e2e74b1241b3c0b0241c2757cd12eb879d085a5
kraksasha <kraksasha@yandex.ru>
2021-11-28 09:46:29 +0300

    eatcat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -36,4 +36,5 @@ public class Cat {
     public void setSatiety(boolean satiety) {
         this.satiety = satiety;
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,4 +17,5 @@ public class Main {
             plate.info();
         }
     }
+
 }
